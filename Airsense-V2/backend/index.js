f5dc22baf3eda507688fa5eb2dc88345a5e86cce
Amelia <ibarguen.luz@correounivalle.edu.co>
2025-11-03 16:26:16 -0500

    Arreglos para que se incluya el visor #4

--- Airsense-V2/backend/index.js
+++ Airsense-V2/backend/index.js
@@ -26,11 +26,13 @@ const PORT = process.env.PORT || 3000; //Usa variable de entorno PORT (producci�
 app.use(cors());                                            //permite que el fronted haga peticiones al backend desde diferente origen
 app.use(express.json());                                    //habilita el procesamiento de peticiones en formato JSON
 
+// 2. ¡AÑADIR ESTA LÍNEA! Permite servir archivos estáticos desde AirSense (para CSS, JS, imágenes del visor)
+app.use(express.static(path.join(__dirname, "../AirSense")));
+
+
 // 1. Permite servir archivos estáticos desde la carpeta public
 app.use(express.static(path.join(__dirname, "../public"))); 
 
-// 2. ¡AÑADIR ESTA LÍNEA! Permite servir archivos estáticos desde AirSense (para CSS, JS, imágenes del visor)
-app.use(express.static(path.join(__dirname, "../AirSense")));
 
 // ==========================================================================
 // UTILIDAD: MANEJADOR ASÍNCRONO DE ERRORES (apiHandler)
