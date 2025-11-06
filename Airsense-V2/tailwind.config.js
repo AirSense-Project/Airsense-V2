37adaa881cf569999e327b1f38faf20b41959b3b
Amelia <ibarguen.luz@correounivalle.edu.co>
2025-11-06 05:19:09 -0500

    Reestructuración de carpetas public/

--- Airsense-V2/tailwind.config.js
+++ Airsense-V2/tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  "./public/index.html",
-  "./public/rutas.js",
-  "./public/mapa.html"
-  ],
+  "./public/index.html",  // Escanea la nueva página de inicio
+  "./public/rutas.js",    // Escanea el JS de la inicio
+  "./public/mapa.html",   // Escanea la página del mapa
+  "./public/script.js"    // Escanea el JS del mapa
+],
   theme: {
     extend: {},
   },
