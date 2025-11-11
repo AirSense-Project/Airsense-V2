731b85b8c9799714cc5c67974998c00de79afe77
Amelia <ibarguen.luz@correounivalle.edu.co>
2025-11-11 14:40:37 -0500

    Arreglos del html 3.6

--- Airsense-V2/public/js/uiscript.js
+++ Airsense-V2/public/js/uiscript.js
@@ -406,9 +406,6 @@ function mostrarEstacionesEnMapa(
         }
       ).addTo(map);
 
-      // Popup siempre interactivo
-      marker.bindPopup(crearPopupInteractivo(est, anio));
-
       if (conInteractividad) {
         marker.on("click", () => {
           window.sincronizarEstacionConSelector(est.id_estacion);
